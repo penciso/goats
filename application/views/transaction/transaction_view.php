8a1fe8b00147bd4bf7153f2883395a02b72d6c87
penciso <penciso@gbox.adnu.edu.ph>
2018-11-20 06:20:40 +0800

    Create transaction_view.php

---
+++ application/views/transaction/transaction_view.php
@@ -0,0 +1,16 @@
+<div class="container-fluid">
+
+  <main role="main">
+    
+     <section>
+      <div class="row">
+        <div class="col-12>
+        
+          
+        </div>
+      </div>
+     </section>
+    
+  </main>
+  
+</div>
